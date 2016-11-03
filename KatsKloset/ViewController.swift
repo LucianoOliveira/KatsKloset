4b888a5fe87150aa24a8dd95e3a91251e992b685
Luciano Oliveira <lucianooliveira@lucianos-air.lan>
2016-11-03 13:00:59 +0000

    Initial Commit

---
+++ KatsKloset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KatsKloset
+//
+//  Created by Luciano Oliveira on 03/11/2016.
+//  Copyright © 2016 Luciano Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
